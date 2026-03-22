26925b2c14e68dec20f813ab0a1e62d4d85f3ef1
petaryordanov998 <petaryordanov998@gmail.com>
2026-03-22 22:49:02 +0200

    Remove secrets.h from repo

--- secrets.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-const char* WIFI_SSID = "Galaxy A34 5G AFAE";
-const char* WIFI_PASSWORD = "12345678";
-
-#define BOT_TOKEN "8631470311:AAG-JU3c7e9ht5mEekceCJYgJc74mLlJyUc"
-#define CHAT_ID "8446141264"
